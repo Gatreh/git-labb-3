63133d46425056dce43d0db9334d5cbe19e91a0b
Gatreh <Gatreh@gmail.com>
2018-09-18 09:47:44 +0000

    re-added some files

--- app/index.php
+++ app/index.php
@@ -1,3 +1,15 @@
+<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Gatreh</title>
+</head>
+<body>
+    <h1>Gatreh was here</h1>
+</body>
+</html>
+=======
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
@@ -9,3 +21,4 @@
     <h1>Gatreh was here</h1>
 </body>
 </html>
+>>>>>>> backend
